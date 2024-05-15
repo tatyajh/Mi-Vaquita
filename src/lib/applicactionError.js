395832cf250f78ea0051daaf8424883ea6c03b0a
tatyajh <talejandra.jaramillo@gmail.com>
2024-05-15 00:07:13 -0500

    files

--- src/lib/applicactionError.js
+++
@@ -1,8 +0,0 @@
-const createApplicationError = (msg, errorCode, cause) => {
-    const error = Error(msg, cause);
-    error.errorCode = errorCode;
-    error.isApplicationError = true;
-    return error; 
-}
-
-export default createApplicationError;
